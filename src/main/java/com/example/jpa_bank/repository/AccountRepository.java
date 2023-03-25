097ebaf9a44a39544bbabd4a1d8621e7bdae6b5c
Jhoan <84868357+Oswe-gif@users.noreply.github.com>
2023-03-25 15:43:47 -0500

    Add query to search all the users' accounts

--- src/main/java/com/example/jpa_bank/repository/AccountRepository.java
+++ src/main/java/com/example/jpa_bank/repository/AccountRepository.java
@@ -7,11 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Transactional
 @Repository
 public interface AccountRepository extends JpaRepository<AccountEntity,Integer> {
     @Modifying
     @Query(value = "update ACCOUNT a set a.MONEY = a.Money+ :money  where a.ID = :idAccount",nativeQuery = true)
     void depositMoney(@Param("money") int money,@Param("idAccount") int idAccount);
-
+    @Query(value ="select * from ACCOUNT where USER = ?1", nativeQuery = true)
+    List<AccountEntity> getAllAccounts(int documentUser);
 }
