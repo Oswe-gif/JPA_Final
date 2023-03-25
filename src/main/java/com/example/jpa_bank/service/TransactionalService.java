b37b1a8490648847d9434c291072a92f7550e52a
Jhoan <84868357+Oswe-gif@users.noreply.github.com>
2023-03-25 02:28:58 -0500

    Add method to deposit money

--- src/main/java/com/example/jpa_bank/service/TransactionalService.java
+++ src/main/java/com/example/jpa_bank/service/TransactionalService.java
@@ -1,5 +1,16 @@
 package com.example.jpa_bank.service;
 
+import com.example.jpa_bank.controller.dto.AccountDto;
+import com.example.jpa_bank.controller.dto.TransactionDto;
+import com.example.jpa_bank.entity.AccountEntity;
+import com.example.jpa_bank.repository.AccountRepository;
+import com.example.jpa_bank.repository.TransactionRepository;
+import com.example.jpa_bank.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class TransactionalService {
-    //programming
+
 }
