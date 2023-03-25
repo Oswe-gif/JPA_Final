936eb7d71b8ab2616ad2baa39b71716e2ee10194
Jhoan <84868357+Oswe-gif@users.noreply.github.com>
2023-03-24 21:02:38 -0500

    Add entity and transaction repository

---
+++ src/main/java/com/example/jpa_bank/entity/TransactionEntity.java
@@ -0,0 +1,32 @@
+package com.example.jpa_bank.entity;
+
+import java.math.BigInteger;
+import java.util.Set;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "TRANSACTION")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class TransactionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String origen;
+    private String destination;
+    private int amount;
+    private BigInteger transaccionId;
+    private BigInteger cuentaDestino;
+    private BigInteger cuentaOrigen;
+    private int monto;
+
+
+}
